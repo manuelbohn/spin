f23bce266dcfc5f979fa2551237b82f7bf9fa101
manuelbohn <bohn@stanford.edu>
2019-02-28 10:41:15 -0800

    add timeout before selection to kids me exp

--- experiments/kids/kids_me.js
+++ experiments/kids/kids_me.js
@@ -1,5 +1,5 @@
 // preload
-var preObjects = ["duck.png","car.png","bear.png","ball.png","t01.png", "t02.png", "t03.png", "t04.png", "t05.png", "t06.png", "t07.png", "t08.png", "t09.png", "t10.png", "t11.png", "t12.png", "t13.png", "t14.png", "t15.png", "t16.png", "t17.png", "t18.png","back1.jpg","back2.jpg","back3.jpg","back4.jpg","back5.jpg","back6.jpg","back7.jpg","back8.jpg","back9.jpg","back10.jpg","empty.png"];
+var preObjects = ["pawn.png","horseshoe.png","carrot.png","barrel.png","papaya.png","bread.png","apple.png","duck.png","garlic.png","kite.png","eggplant.png","plug.png","duck.png","car.png","bear.png","ball.png","t01.png", "t02.png", "t03.png", "t04.png", "t05.png", "t06.png", "t07.png", "t08.png", "t09.png", "t10.png", "t11.png", "t12.png", "t13.png", "t14.png", "t15.png", "t16.png", "t17.png", "t18.png","t20.png","t21.png","back1.jpg","back2.jpg","back3.jpg","back4.jpg","back5.jpg","back6.jpg","back7.jpg","back8.jpg","back9.jpg","back10.jpg","empty.png"];
 //for critical trials and fillers
 var images = new Array();
 for (i = 0; i < preObjects.length; i++) {
