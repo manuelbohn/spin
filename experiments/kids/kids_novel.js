90ae09902f082f39ce2125b841513aa513d9c78c
manuelbohn <bohn@stanford.edu>
2019-03-29 13:18:05 -0700

    kids novelty exp

--- experiments/kids/kids_novel.js
+++ experiments/kids/kids_novel.js
@@ -394,13 +394,13 @@ pause: function () {
     };
     
     
-    // choice can be made by clicking the objects after - possible after 8s
+    // choice can be made by clicking the objects after - possible after 5s
     setTimeout(function() {
         $(".object_l").click(experiment.eat);
 
         $(".object_r").click(experiment.eat);
 
-    }, 000);
+    }, 5000);
   },
         
 // moving on within a trial
